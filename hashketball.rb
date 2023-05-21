e3078440a209863990589bc83367f2992f88cd8c
Ruth <rwangare124@gmail.com>
2023-05-21 15:46:23 +0300

    Completed assignment

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,60 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored(player_name)
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
+      return player[:points] if player[:player_name] == player_name
+    end
+  end
+end
+
+def shoe_size(player_name)
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
+      return player[:shoe] if player[:player_name] == player_name
+    end
+  end
+end
+
+def team_colors(team_name)
+  game_hash.each do |location, team_data|
+    return team_data[:colors] if team_data[:team_name] == team_name
+  end
+end
+
+def team_names
+  game_hash.map { |location, team_data| team_data[:team_name] }
+end
+
+def player_numbers(team_name)
+  game_hash.each do |location, team_data|
+    if team_data[:team_name] == team_name
+      return team_data[:players].map { |player| player[:number] }
+    end
+  end
+end
+
+def player_stats(player_name)
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
+      return player if player[:player_name] == player_name
+    end
+  end
+end
+
+def big_shoe_rebounds
+  max_shoe_size = 0
+  rebounds = 0
+
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
+      if player[:shoe] > max_shoe_size
+        max_shoe_size = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+
+  rebounds
+end
